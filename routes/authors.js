f2c4d624ae929eaf0b26df6809f05771fdc5ade7
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-04-21 23:52:12 +0100

    initial author routes

--- routes/authors.js
+++ routes/authors.js
@@ -3,18 +3,39 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // all authors route
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name !== null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', { authors: authors, searchOptions: req.query })
+  } catch {
+    res.redirect('/')
+  }
 })
 
 //new author route
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() })
+  res.render('authors/new', { author: new Author() })
 })
 
 // create author route
-router.post('/', (req, res) => {
-    res.send(req.body.name)
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author',
+    })
+  }
 })
 
 module.exports = router
